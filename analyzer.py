5abc826ae37c9f859a907fd3dd9f2c37717ad3d9
EGA-SUPREMO <cxiesestro@gmail.com>
2023-12-18 14:44:52 -0400

    add subwrapper

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,9 @@
 from pysubparser import parser
 
 def read_subs(input_1: str, input_2: str):
-    return parser.parse(input_1), parser.parse(input_2)
-    
+    return list(parser.parse(input_1)), list(parser.parse(input_2))
 
 subtitles1, subtitles2 = read_subs("subs_en.srt", "subs_indo.srt")
-for subtitle in subtitles1:
-    print(subtitle.text)
-print(subtitles1)
-print(subtitles2)
+
+print(subtitles1[0])
+print(subtitles2[0])
---
+++ subtitle_wrapper.py
@@ -0,0 +1,7 @@
+
+class SubtitleWrapper:
+    def __init__(self, subtitle_line, id_line_external, grade_level, is_valid):
+        self.subtitle_line = subtitle_line
+        self.id_line_external = id_line_external
+        self.grade_level = grade_level
+        self.is_valid = is_valid 
