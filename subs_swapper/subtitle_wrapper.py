fac103033f77db74b1112eb6ecf0279e1e9f30c4
EGA-SUPREMO <cxiesestro@gmail.com>
2023-12-26 11:40:56 -0400

    add __repr__ method to subswapper

--- subs_swapper/subtitle_wrapper.py
+++ subs_swapper/subtitle_wrapper.py
@@ -6,3 +6,6 @@ class SubtitleWrapper:
         self.grade_level = grade_level
         self.is_valid = is_valid
         self.swap = False 
+
+    def __repr__(self):
+        return f"{self.subtitle_line.index} > {self.id_line_external} : {self.subtitle_line.text}"
